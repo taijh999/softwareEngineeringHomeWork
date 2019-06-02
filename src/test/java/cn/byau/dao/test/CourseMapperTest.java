b24e00125e39c9c2ebd4da899d66691cb57e09d4
tjh <taijh999@163.com>
2019-06-02 23:32:35 +0800

    将spring-mybatis.xml中的classpath改为classpath*

--- src/test/java/cn/byau/dao/test/CourseMapperTest.java
+++ src/test/java/cn/byau/dao/test/CourseMapperTest.java
@@ -8,12 +8,12 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.junit.Assert;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 import cn.byau.dao.CourseMapper;
 import cn.byau.pojo.Course;
@@ -21,10 +21,10 @@ import cn.byau.pojo.Course;
 @RunWith(SpringJUnit4ClassRunner.class)
 
 /** 声明spring主配置文件位置，注意：以当前测试类的位置为基准,有多个配置文件以字符数组声明 **/
-@ContextConfiguration(locations={"classpath:config/spring-core.xml"})
+@ContextConfiguration(locations={"classpath:config/spring-core.xml","classpath:config/spring-tx.xml"})
 
 /** 声明使用事务，不声明spring会使用默认事务管理 **/
-//@Transactional
+@Transactional
 
 /** 声明事务回滚，要不删除一个数据就没有了，注意：插入数据时可注掉，不让事务回滚 **/
 //@TransactionConfiguration(transactionManager="transactionManager",defaultRollback=true)
@@ -69,21 +69,14 @@ public class CourseMapperTest {
 	}
 
 	@Test
-	@Ignore
 	public void testInsertBatch() {
 		List<Course> courseList = new ArrayList<>();
-		courseList.add(new Course("1060", "java1", "10", "5", "90", "课程介绍1"));
-		courseList.add(new Course("1061", "java1", "10", "5", "90", "课程介绍1"));
+		courseList.add(new Course("1090", "java1", "10", "5", "90", "课程介绍1"));
+		courseList.add(new Course("1091", "java1", "10", "5", "90", "课程介绍1"));
 		int x = courseDao.insertBatch(courseList);
 		assertEquals(2, x);
 	}
 
-	@Test
-	public void testDeleteBatch() {
-		String courseIds[] = new String[] {"1061,1020"};
-		//List<String> courseIds1=Arrays.asList(courseIds);
-		int x = courseDao.deleteBatch(courseIds);
-		assertEquals(2, x);
-	}
+	
 
 }
